0f1d576f9ac013563b15092e60ba5b5b534c7f3b
vodangkick <quang115thd@gmail.com>
2020-07-15 04:24:09 -0700

    Updates

---
+++ precache-manifest.3592ac51a70709bdf51a9b6bfed25b71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54cbbf52ab5682a40ca3a07eecefe387",
+    "url": "/dog-cat-deme/index.html"
+  },
+  {
+    "revision": "c355aacdafa16a02485a",
+    "url": "/dog-cat-deme/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "99f079a6f1731ebe042b",
+    "url": "/dog-cat-deme/static/js/2.766addb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog-cat-deme/static/js/2.766addb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c355aacdafa16a02485a",
+    "url": "/dog-cat-deme/static/js/main.6f9e9f82.chunk.js"
+  },
+  {
+    "revision": "51cbdac19f378d085e22",
+    "url": "/dog-cat-deme/static/js/runtime-main.c3912c57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dog-cat-deme/static/media/logo.5d5d9eef.svg"
+  }
+]);
